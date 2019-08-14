53ea90c1f32bc78bc2edf11d91e50f7be9c572b7
zarar2312 <zararefraimhotmail.com@gmail.com>
2019-08-14 10:45:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,29 +8,438 @@ const moment = require('moment');
 require('./util/eventLoader')(client);
 
 var prefix = ayarlar.prefix;
+var role = ayarlar.role;
+var Long = require("long");
 
 const log = message => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
 
-client.on("ready", () => {
-  client.user.setGame(prefix + "yardım | Sunucu Korunuyor.. |          🚀Space Realm: Mars🚀")
-  console.log("**Bağlandım**!")
+client.on("guildMemberAdd", (member) => {
+let guild = member.guild; // Reading property `guild` of guildmember object.
+let server = member.count;
+let user = member.user
+let username = member.user.username; // GuildMembers don't have a tag property, read property user of guildmember to get the user object from it
+if(guild.systemChannel){ // Checking if it's not null
+	guild.systemChannel.send('\n **'+(user)+'**, **Space Realm: Marsa, Hoş Geldin !** \n Kayıt olmak içi ses odalarına girebilir misin?');
+}
 });
 
 bot.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'hosgeldiniz-log');
-  if (!channel) return;
-  if(!channel) return message.channel.send(" `hosgeldiniz-log` İsminde Yazı Kanalı Bulamıyorum.!");
+    member.guild.channels.get('608755856934305821').send("Hoş Geldin"); 
+});
 
-  channel.send(`Sunucuya hoşgeldiniz, ${member}`);
-  const sunucubilgi = new Discord.RichEmbed()
-  .setAuthor(`**Aramıza Hoşgeldin.** 🚀 ${member}`)
-  .setColor(3447003)
-  .setTimestamp()
-  .setDescription('')
-  .setImage(`https://cdn.discordapp.com/attachments/584989861719572482/590576147331547148/marsa-hosgeldiniz.gif`)
-  return message.channel.sendEmbed(sunucubilgi);
+client.on('ready', () => {
+  let channel = client.channels.get('609288972463636481');
+  channel.join()
+});
+
+client.on('message', msg => {
+  if (msg.content === 'amk') {
+    client.channels.get(608734984383102995)
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+
+    exports.conf = {
+      enabled: true,
+      guildOnly: true,
+      aliases: ['amk'],
+      permLevel: 2
+    };
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'aq') {
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!p https://discord.gg') {
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!p') {
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'orospu cocugu') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ana skm') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'anan sikim') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+//selamün aleyküm
+client.on('message', msg => {
+  if (msg.content === 'discord.gg') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+//ᴀᴍᴋ
+client.on('message', msg => {
+  if (msg.content === 'ᴀᴍᴋ') {
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'sa') {
+          if (!msg.guild.member(msg.author).hasPermission("SEND_MESSAGES")) {
+          msg.react("🇦");
+          msg.react("🇸");           
+          } else {
+          msg.react("🇦");       
+          msg.react("🇸");
+          }
+      }
+  });
+
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'Selam') {
+          if (!msg.guild.member(msg.author).hasPermission("SEND_MESSAGES")) {
+          msg.react("🇦");
+          msg.react("🇸");           
+          } else {
+          msg.react("🇦");       
+          msg.react("🇸");
+          }
+      }
+  });
+
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'slm') {
+          if (!msg.guild.member(msg.author).hasPermission("SEND_MESSAGES")) {
+          msg.react("🇦");
+          msg.react("🇸");           
+          } else {
+          msg.react("🇦");       
+          msg.react("🇸");
+          }
+      }
+  });
+
+client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'Selamun Aleküm') {
+          if (!msg.guild.member(msg.author).hasPermission("SEND_MESSAGES")) {
+          msg.react("🇦");
+          msg.react("🇸");           
+          } else {
+          msg.react("🇦");       
+          msg.react("🇸");
+          }
+      }
+  });
+//ᴀǫ
+client.on('message', msg => {
+  if (msg.content === 'ᴀǫ') {
+    msg.delete(5)
+    msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'Selamun aleküm') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'selamün aleküm') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Selamün aleküm') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Selam') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', message => {
+if (message.content.toLowerCase() === prefix + "beynim") {
+    var sans = ["11", "15", "20", "24", "28", "31", "39", "45", "49", "54", "58", "63", "67", "77", "73", "84", "80", "83", "96", "94", "99", "Albert Einstein mısın krdşm"];
+    var sonuc = sans[Math.floor((Math.random() * sans.length))];
+    const embed = new Discord.RichEmbed()
+    .addField(`**IQ Seviyen💡**`, `${sonuc}`)
+    return message.channel.sendEmbed(embed);
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === '!tag') {
+    msg.channel.send('⸘');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!link') {
+    msg.channel.send('`Sınırsız Mars Seyehat Bileti:`https://discord.gg/Z9K9gC6 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'slm') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Slm') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+    setTimeout(function(){
+}, 1000);
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Sea') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sea') {
+    msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'SEA') {
+    msg.reply(' `Aleyküm Selam Hoş Geldin🚀`');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Selamm') {
+      msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sᴇʟᴀᴍᴜɴ ᴀʟᴇʏᴋᴜᴍ') {
+      msg.reply('**Aleyküm Selam Hoş Geldin** 🚀');
+}
+});
+//sᴀʟᴀᴋ
+client.on('message', msg => {
+  if (msg.content === 'sᴀʟᴀᴋ') {
+    msg.reply('**Sende MALSIN** ! 💡');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Salak') {
+    msg.reply('**Sende MALSIN** ! 💡');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Salak') {
+    msg.reply('**Sende MALSIN** ! 💡');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'SALAK') {
+    msg.reply('**Sende MALSIN** ! 💡');
+  }
+});
+//ᴍᴀʟ
+client.on('message', msg => {
+  if (msg.content === 'Mal') {
+    msg.reply('**Sende Salaksın** ! 💡');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ᴍᴀʟ') {
+    msg.reply('**Sende Salaksın** ! 💡');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Günaydın') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+//ɢᴜɴᴀʏᴅɪɴ
+client.on('message', msg => {
+  if (msg.content === 'ɢᴜɴᴀʏᴅɪɴ') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'günaydın') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Gunaydn') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'GÜNAYDIN') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'GUNAYDIN') {
+    msg.reply('**Günaydın Kaptan** ! `Hoş Geldin` 🚀');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Tünaydın') {
+      msg.reply('**Tünaydın Kaptan** ! `Hoş Geldin` 🚀');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'tünaydın') {
+      msg.reply('**Tünaydın Kaptan** ! `Hoş Geldin` 🚀');
+}
+});
+//bot naber
+client.on('message', msg => {
+  if (msg.content === 'bot naber') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Bot naber') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Bot Naber') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'Nasılsın') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Sen nasılsın') {
+      msg.reply(' `Çok iyiyim burası çok yalnız hissetiriyor..` 🎈');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sen nasılsın') {
+      msg.reply(' `Çok iyiyim burası çok yalnız hissetiriyor..` 🎈');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Sen Nasılsın') {
+      msg.reply(' `Çok iyiyim burası çok yalnız hissetiriyor..` 🎈');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Naber') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'naber') {
+      msg.reply(' `İyiyim siz nasılsınız?` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'iyi') {
+      msg.reply(' `İyi olmana sevidim!` 😍');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'kötü') {
+      msg.reply(' `Kötü olmana üzüldüm kaptan.` 🤔');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'napıyon') {
+      msg.reply(' `İyidir Sunucuya Bakıyorum.` 🖤');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Oc') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+      msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === '!p discord.gg') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+      msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === '!p https://discord.gg') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+      msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === '!phttps://discord.gg') {
+    client.channels.get(597360780605652998)
+    msg.delete(5)
+      msg.reply(' `Lütfen şunu kes! Başka işler ile ilgilenir misin?` ');
+}
 });
 
 client.commands = new Discord.Collection();
@@ -101,7 +510,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-      msg.reply('**AleykümSelam Hoşgeldin** 🚀');
+      msg.reply(' `Aleyküm Selam Hoş Geldin!`🚀');
 }
 });
 
@@ -116,9 +525,6 @@ client.elevation = message => {
 };
 
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// client.on('debug', e => {
-//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
-// });
 
 client.on('warn', e => {
   console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
@@ -128,4 +534,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(ayarlar.token);
